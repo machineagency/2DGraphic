bcb1a15b3ff40c74dc7d77bd8964dc4690d84c02
yu zhang <zzyoctopus@gmail.com>
2023-04-12 21:41:53 -0700

    cm

--- p3-graphicTool/mouthInterface.js
+++ p3-graphicTool/mouthInterface.js
@@ -1,3 +1,4 @@
+import { Point } from "paper/dist/paper-core";
 import {
   placeArrow,
   placeArc,
@@ -12,13 +13,52 @@ window.onload = function () {
   let canvas = document.getElementById("myCanvas");
   p.setup(canvas);
   var children = p.project.activeLayer.children;
+  var canvasTool = new p.Tool();
+
+  var gridPath = new p.Path({
+    name: "gridPath",
+    segments: [[0,0], [1000,1000]],
+    strokeColor: "black",
+  })
+
+  var drawGridLines = function(gridsize, boundingRect, center) {
+    
+    var xPos = center.x;
+    var yPos = center.y;
+    console.log(boundingRect.top);
+
+    // var aLine = new p.Path.Line(new p.Point(boundingRect.left, center.y), new p.Point(boundingRect.right, center.y));
+
+    for (var i = 0; yPos >= boundingRect.top; i++) {
+      yPos -= gridsize;
+      console.log(yPos);
+      var leftPoint = new p.Point(boundingRect.left, yPos);
+      var rightPoint = new p.Point(boundingRect.right, yPos);
+      var aLine = new p.Path.Line(leftPoint, rightPoint);
+      aLine.strokeColor = 'black';
+    }
+    yPos = center.y;
+
+    for (var i = 0; yPos <= boundingRect.bottom; i++) {
+      yPos += gridsize;
+      var leftPoint = new p.Point(boundingRect.left, yPos);
+      var rightPoint = new p.Point(boundingRect.right, yPos);
+      var aLine = new p.Path.Line(leftPoint, rightPoint);
+      aLine.strokeColor = 'black';
+    }
+    yPos = center.y;
+  } 
+  drawGridLines(40, p.view.bounds, p.view.center);
+
 
   var sketchTool = new p.Tool();
+
   var mainPath = new p.Path({
     name: "mainPath",
     strokeColor: "black",
   });
 
+  // var tempLayer = new p.Layer();
   var start, end;
   sketchTool.onMouseDown = function (event) {
     start = placeMouthDown(event.point, p, children);
